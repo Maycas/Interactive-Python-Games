bb51f05165a81c874f5a671c8c628060bb422f3a
Marc Maycas <Marc@MacBook-Pro-de-Marc.local>
2014-04-08 23:36:47 +0200

    New versions from RPSLS and Guess the Number

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -7,91 +7,70 @@ import random
 import math
 
 # initialize global variables used in your code
-high = 100
 low = 0
-computer_guess = random.randrange(low, high)
-remaining = math.ceil(math.log(high - low + 1, 2))
+high = 100
 
-# helper function to initialize the game
-def init():
-    global f
-    global remaining
-    f.start()
-    print "New Game. Range is from", low, "to", high
-    print "Number of remaining guesses is:", remaining, "\n"
-
-# helper function to restart the game once lost or won
-def restart(high):
-    # the game must be restarted once won
-    # game selection depends on the current value of variable high
-    if high == 100:
-        range100()
-    else:
-        range1000()
-
-# define event handlers for control panel
+# helper function to start and restart the game
+def new_game():
+    # set (or resets) the number, the remaining guesses
+    # and starts (or restarts) a new game
+    global number, remaining
+    number = random.randrange(low, high)
+    remaining = int(math.ceil(math.log(high - low + 1, 2)))
     
+    # show game status
+    print "New game. Range is from", low, "to", high
+    print "Number of remaining guesses is", remaining, "\n"
+    
+# define event handlers for control panel
 def range100():
-    # button that changes range to range [0,100) and restarts
-    # resets the number limits from 0 to 100
-    # sets remaining according to 2 ** remaining >= high - low + 1
-    global computer_guess
-    global low
-    global high
-    global remaining
-    low = 0
+    # button that changes range to range [0,100) 
+    # and restarts the remaining guesses and the game
+    global high, remaining
     high = 100
-    remaining = math.ceil(math.log(high - low + 1, 2))
-    print "New Game. Range is from", low, "to", high
-    print "Number of remaining guesses is:", remaining, "\n"
-    computer_guess = random.randrange(low, high)
+    remaining = int(math.ceil(math.log(high - low + 1, 2)))
+    new_game()    
 
 def range1000():
-    # button that changes range to range [0,1000) and restarts
-    # resets the number limits from 0 to 1000
-    # sets remaining according to 2 ** remaining >= high - low + 1
-    global computer_guess
-    global low
-    global high
-    global remaining
-    low = 0
+    # button that changes range to range [0,1000) 
+    # # and restarts the remaining guesses and the game
+    global high, remaining
     high = 1000
-    remaining = math.ceil(math.log(high - low + 1, 2))
-    print "New Game. Range is from", low, "to", high
-    print "Number of remaining guesses is:", remaining, "\n"
-    computer_guess = random.randrange(low, high)
-    
-def get_input(guess):
+    remaining = int(math.ceil(math.log(high - low + 1, 2)))
+    new_game()
+        
+def input_guess(guess):
     # main game logic goes here
-    # retrieves the guess from the input and compares it with computer_guess
-    global computer_guess
-    global remaining
-    global high
-    global low
-    guess = float(guess)
-    remaining = remaining - 1
-    print "Guess was", guess
-    print "Number of remaining guesses is:", remaining
-    if guess == computer_guess and remaining >= 0:
-        print "Correct! \n"
-        restart (high)
-    elif guess < computer_guess and remaining != 0:
-        print "Higher! \n"
-    elif guess > computer_guess and remaining != 0:
-        print "Lower! \n"
-    else:
-        print "You ran out of guesses. The number was", computer_guess, "\n"
-        restart (high)
+    global number, remaining
     
+    # decrease the number of remaining guesses
+    remaining -= 1
+    
+    # show game status
+    print "Guess was", guess
+    print "Number of remaining guesses is", remaining
+    
+    # compare the introduced input, show appropiate message
+    # and restart the game if it has ended
+    if int(guess) < number and remaining > 0:
+        print "Higher!\n"
+    elif int(guess) > number and remaining > 0:
+        print "Lower!\n"
+    elif int(guess) == number and remaining >=0:
+        print "Correct!", "\n\n"
+        new_game()
+    else:
+        print "You ran out of guesses. The number was", number, "\n\n"
+        new_game()
+        
 # create frame
-f = simplegui.create_frame("Guess the number", 200, 200)
+frame = simplegui.create_frame("Guess the number", 200, 200)
 
 # register event handlers for control elements
-f.add_button("Range is [0,100)", range100, 200)
-f.add_button("Range is [0,1000)", range1000, 200)
-f.add_input("Enter a guess", get_input, 200)
+frame.add_input("Enter your guess", input_guess, 200)
+frame.add_button("Range 0 - 100", range100, 200)
+frame.add_button("Range 0 - 1000", range1000, 200)
 
-# start frame
-init()
-
-# always remember to check your completed program against the grading rubric
+# call new_game and start frame
+new_game()
+frame.start()
--- RPSLS.py
+++ RPSLS.py
@@ -11,88 +11,77 @@
 # 3 - lizard
 # 4 - scissors
 
-# Required libraries
+# libraries required
 import random
 
 # helper functions
 
+def name_to_number(name):
+    # convert name to number using if/elif/else
+    # don't forget to return the result!
+    if name == 'rock':
+        return 0
+    elif name == 'Spock':
+        return 1
+    elif name == 'paper':
+        return 2
+    elif name == 'lizard':
+        return 3
+    elif name == 'scissors':
+        return 4
+    else:
+        return 'Incorrect name. Please review the name spelling'
+
 def number_to_name(number):
-    # fill in your code below
     # convert number to a name using if/elif/else
     # don't forget to return the result!
-    if (number == 0):
+    if number == 0:
         return 'rock'
-    elif (number == 1):
+    if number == 1:
         return 'Spock'
-    elif (number == 2):
+    if number == 2:
         return 'paper'
-    elif (number == 3):
+    if number == 3:
         return 'lizard'
-    elif (number == 4):
+    if number == 4:
         return 'scissors'
     else:
         return "Incorrect number. The number must be in the range between 0 to 4"
-
     
-def name_to_number(name):
-    # fill in your code below
-    # convert name to number using if/elif/else
-    # don't forget to return the result!
-    if (name == 'rock'):
-        return 0
-    elif (name == 'Spock'):
-        return 1
-    elif (name == 'paper'):
-        return 2
-    elif (name == 'lizard'):
-        return 3
-    elif (name == 'scissors'):
-        return 4
-    else:
-        return "Incorrect name. Please review the name spelling"
 
-
-def rpsls(name): 
-    # fill in your code below
-    # convert name to player_number using name_to_number
-    player_number = name_to_number(name)
-
-    # compute random guess for comp_number using random.randrange() 
-    comp_number = random.randrange(0,5)
-    # As stated in the documentation, randrange computes a random number 
-    # in the range 0 <= N < 5. So, in this example it will compute a number 
-    # from 0 to 4, what expected
-
-    # compute difference of player_number and comp_number modulo five
-    difference = (player_number - comp_number) % 5
-
-    # use if/elif/else to determine winner
-    if (difference == 1) or (difference == 2):
-        winner = 'Player'
-    elif (difference == 3) or (difference == 4):
-        winner = 'Computer'
-    else:
-        winner = 'Tie'
-
-    # convert comp_number to name using number_to_name
-    comp_name = number_to_name (comp_number)
-    
-    # print results
-    print 'Player chooses', name
-    print 'Computer chooses', comp_name
-    if (winner != 'Tie'):
-        print winner, 'wins!'
-    else:
-        print 'Player and computer tie!'
+def rpsls(player_choice): 
+    # print a blank line to separate consecutive games
     print ''
     
-# test your code
+    # print out the message for the player's choice
+    print 'Player chooses', player_choice
+
+    # convert the player's choice to player_number using the function name_to_number()
+    player_number = name_to_number(player_choice)
+    
+    # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(0, 5)
+
+    # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
+    
+    # print out the message for computer's choice
+    print 'Computer chooses', comp_choice
+
+    # compute difference of comp_number and player_number modulo five
+    difference = (player_number - comp_number) % 5
+    
+    # use if/elif/else to determine winner, print winner message
+    if difference == 1 or difference == 2:
+        print 'Player wins!'
+    elif difference == 3 or difference == 4:
+        print 'Computer wins!'
+    else:
+        print 'Player and computer tie!'
+    
+# test your code - LEAVE THESE CALLS IN YOUR SUBMITTED CODE
 rpsls("rock")
 rpsls("Spock")
 rpsls("paper")
 rpsls("lizard")
 rpsls("scissors")
-
-# always remember to check your completed program against the grading rubric
-
-
